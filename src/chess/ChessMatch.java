31dde73143749d1d7a48fafcfdb0f0a806a36363
Leo Riegler <leo.riegler1@gmail.com>
2020-03-11 15:17:57 -0300

    Movimento possíveis de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -42,6 +42,8 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe peça na posição de origem");
+		}if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existe nenhum movimento possível");
 		}
 	}
 	
